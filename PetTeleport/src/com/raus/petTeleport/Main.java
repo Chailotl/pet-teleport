9e49385fbc89089759ef2cb14b119b44878ddb69
Chailotl <7232280+Chailotl@users.noreply.github.com>
2020-04-20 19:03:23 -0400

    First upload

---
+++ PetTeleport/src/com/raus/petTeleport/Main.java
@@ -0,0 +1,19 @@
+package com.raus.petTeleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		// Listeners
+		getServer().getPluginManager().registerEvents(new UnloadListener(), this);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
